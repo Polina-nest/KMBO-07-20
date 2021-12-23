8877a3b36645994b279b407a2da17984d52cdcb5
Polina-nest <46245789+Polina-nest@users.noreply.github.com>
2021-12-23 21:17:19 +0300

    Update task1.jl

--- Kiselyov A.A/task1.jl
+++ Kiselyov A.A/task1.jl
@@ -1,22 +1,31 @@
-inverse(origSide::HorizonSide) = HorizonSide(mod(Int(origSide) + 2, 4))
-function moveSide(r, side)
-    x = 0
-    while isborder(r, side) == false
-        move!(r, side)
+function mark_kross!(r::Robot)
+    while isborder(r,Nord)==false
+        move!(r,Nord)
         putmarker!(r)
-        x += 1
     end
-
-    while x > 0
-        move!(r, inverse(side))
-        x -= 1
+    while ismarker(r)==true
+        move!(r,Sud)
     end
-end
-
-function main1!(r::Robot)
-    moveSide(r, Nord)
-    moveSide(r, Sud)
-    moveSide(r, Ost)
-    moveSide(r, West)
+    while isborder(r,Sud)==false
+        move!(r,Sud)
+        putmarker!(r)
+    end
+    while ismarker(r)==true
+        move!(r,Nord)
+    end
+    while isborder(r,Ost)==false
+        move!(r,Ost)
+        putmarker!(r)
+    end
+    while ismarker(r)==true
+        move!(r,West)
+    end
+    while isborder(r,West)==false
+        move!(r,West)
+        putmarker!(r)
+    end
+    while ismarker(r)==true
+        move!(r,Ost)
+    end
     putmarker!(r)
 end
